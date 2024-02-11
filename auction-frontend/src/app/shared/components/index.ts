292a2f0abcecb4bc9953e1cd12faa98060269649
Artem Petselia <mrtimego@ukr.net>
2024-02-11 19:09:49 +0200

    footer done

--- auction-frontend/src/app/shared/components/index.ts
+++ auction-frontend/src/app/shared/components/index.ts
@@ -3,5 +3,6 @@ export * from './header/header.component';
 export * from './image-carousel/image-carousel.component';
 export * from  './lot-card/lot-card.component';
 export * from './auth-form/auth-form.component';
+export * from './footer/footer.component';
 
 export * from './controls';
