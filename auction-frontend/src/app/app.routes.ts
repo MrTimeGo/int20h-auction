47631b60282551bcc6ae7d2f7f50e54753bedaf3
Artem Petselia <mrtimego@ukr.net>
2024-02-11 19:22:03 +0200

    Added auth guard

--- auction-frontend/src/app/app.routes.ts
+++ auction-frontend/src/app/app.routes.ts
@@ -4,13 +4,14 @@ import { NewLotComponent } from './views/new-lot/new-lot.component';
 import { SignInComponent } from './views/sign-in/sign-in.component';
 import { SignUpComponent } from './views/sign-up/sign-up.component';
 import { LotDetailsComponent } from './views/lot-details/lot-details.component';
+import { authGuard } from './shared/guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
-  { path: 'lots', component: LotComponent },
-  { path: 'lots/:id', component: LotDetailsComponent },
-  { path: 'edit/:id', component: NewLotComponent },
-  { path: 'new', component: NewLotComponent },
+  { path: '', redirectTo: '/lots', pathMatch: 'full' },
+  { path: 'lots', component: LotComponent, canActivate: [authGuard] },
+  { path: 'lots/:id', component: LotDetailsComponent, canActivate: [authGuard] },
+  { path: 'edit/:id', component: NewLotComponent, canActivate: [authGuard] },
+  { path: 'new', component: NewLotComponent, canActivate: [authGuard] },
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent },
 ];
